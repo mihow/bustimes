d92421e60146048924a2f0aea987cd1dcb662161
Michael Bunsen <notbot@gmail.com>
2016-11-27 22:42:25 -0800

    Fix log script (`watch` doesn't allow for a dynamic date value)

--- bustimes/log_bus_times.sh
+++ bustimes/log_bus_times.sh
@@ -4,6 +4,18 @@ source env/bin/activate
 
 mkdir -p data
 
-FILENAME="./data/bustimes__$(date +%Y-%m-%d__%H-%M-%S).json"
+INTERVAL=60
 
-watch --interval 60 "python2.7 bustimes.py > $FILENAME"
+while true
+do
+    FILENAME=./data/bustimes__$(date +%Y-%m-%d__%H-%M-%S).json
+    clear
+    echo "Writing bus data to file: $FILENAME"
+    echo
+    echo "Interval: $INTERVAL seconds"
+    echo
+    echo "Summary:"
+    echo
+	python2.7 bustimes.py > $FILENAME
+    sleep $INTERVAL
+done
