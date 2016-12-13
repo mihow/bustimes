4aee2e8c604ac2d5e9f3ee9c4d5ffc21b3a4620d
Michael Bunsen <notbot@gmail.com>
2016-12-13 13:42:57 -0800

    Adjust outliers

--- find_completed_routes.py
+++ find_completed_routes.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 
+import argparse
+
 import pandas as pd
 import numpy as np
 import datetime as dt
 
-df = pd.read_json('bus_19_history.json')
+parser = argparse.ArgumentParser()
+parser.add_argument("path_to_bus_trip_json_data")
 
 # df.pivot_table(index=['tripID'], values=['event_day'], aggfunc='count').sort_values(by='event_day')
 # 
@@ -66,145 +69,151 @@ def distance_from_observation_to_last_stop(row):
     pass
 
 
-df['event_timestamp'] = df.event_timestamp.apply(get_datetime)
+def main(df, save=True):
+    df['event_timestamp'] = df.event_timestamp.apply(get_datetime)
 
-#df['weekday'] = df.event_timestamp.apply(get_weekday)
+    #df['weekday'] = df.event_timestamp.apply(get_weekday)
 
-# monday_trips = df[df.weekday == 'Monday']
+    # monday_trips = df[df.weekday == 'Monday']
 
-# vehicles = df.pivot_table(index=['vehicle_id'], values=['situation'], aggfunc=lambda x: len(x.unique())).sort_values(by='situation')
-
-
-# Get only data points right after the bus leaves a stop (first or last stop)
-leavings = df[df.situation.str.contains('leaving')]
-
-# Work with a subset of columns
-# leavings_simple = leavings[['vehicle_id', 'event_timestamp', 'situation']]
-
-# Group by vehicle ID so our timeseries data makes sense
-
-# vehicles = leavings.groupby('vehicle_id')
-
-# Sample group of data points for a single vehicle
-# k, v = list(vehicles)[2]
-# k # vehicle_id
-# v # dataframe
+    # vehicles = df.pivot_table(index=['vehicle_id'], values=['situation'], aggfunc=lambda x: len(x.unique())).sort_values(by='situation')
 
 
-# all_routes_dataframes = []
-# all_blocks = set()
-# 
-# # @TODO for later
-# for k, v in vehicles:
+    # Get only data points right after the bus leaves a stop (first or last stop)
+    leavings = df[df.situation.str.contains('leaving')]
 
-def stats_for_vehicle_groups(df):
-    df = df.sort_values('event_timestamp')
+    # Work with a subset of columns
+    # leavings_simple = leavings[['vehicle_id', 'event_timestamp', 'situation']]
 
-    # Save only the rows where the situation/state changed (approaching => leaving)
-    state_changes = (df != df.shift(1)).situation
-    vroutes = df.loc[state_changes].copy()
+    # Group by vehicle ID so our timeseries data makes sense
 
-    previous_rows = vroutes.shift(1)
+    # vehicles = leavings.groupby('vehicle_id')
 
-    # Save the start time, end time & duration of each trip
-    vroutes['trip_start'] = previous_rows.event_timestamp # Time leaving first stop
-    vroutes['trip_end'] = vroutes.event_timestamp # Time leaving last stop
-    vroutes['duration'] = vroutes.trip_end - vroutes.trip_start
-
-    vroutes['delay_start'] = previous_rows.delay 
-    vroutes['delay_end'] = vroutes.delay
-
-    # Save the block ID for the start and end of each trip
-    # These should always be the same, otherwise something is wrong
-    # with that data point.
-    vroutes['block_start'] = previous_rows.blockID.astype('str')
-    vroutes['block_end'] = vroutes.blockID.astype('str')
-
-    # Calculte the distance between each start and and point
-    # This should help validate data as well 
-    vroutes['lat_start'] = previous_rows.vehicle_location_latitude
-    vroutes['lon_start'] = previous_rows.vehicle_location_longitude
-    vroutes['lat_end'] = vroutes.vehicle_location_latitude
-    vroutes['lon_end'] = vroutes.vehicle_location_longitude
-    vroutes['distance'] = vroutes.apply(get_distance, axis=1)
-
-    # Only keep the "ending" data points for each trip
-    completed_routes = vroutes[vroutes.situation.str.contains('last')]
-
-    # Only keep columns we are interested in, and order them
-    completed_routes = completed_routes[[
-            'duration',
-            'distance',
-            'delay', # at last stop observation
-            'trip_start',
-            'trip_end',
-            'block_start',
-            'block_end',
-            'vehicle_id',
-            'type',
-            'tripID',
-            'situation',
-            'routeNumber',
-            'direction',
-            'bearing',
-            'lastLocID',
-            'lastStopSeq',
-            'loadPercentage', # at last stop observation
-            ]]
-
-    # print("Sample for vehicle #{}".format(k))
-    # print(completed_routes)
-
-    # all_routes_dataframes.append(completed_routes)
-    return completed_routes
-
-# routes = pd.concat(all_routes_dataframes)
-routes = leavings.groupby('vehicle_id', group_keys=False).apply(stats_for_vehicle_groups)
-# unstack()?
-
-# Drop rows with NA durations
-routes = routes.dropna()
-
-# Drop outliers
-# about 50 out of 4000 are over 100 minutes
-# @TODO look more into these later
-# routes = routes[routes.duration < dt.timedelta(seconds=(60*100))]
-# routes = routes[scipy.stats.zscore(routes.duration) < 3]
-# These are generally over 4 hours or event multiple days
-routes = routes[routes.duration < routes.duration.quantile(0.98)]
-
-# Add new columns for grouping & aggregating 
-routes['weekday'] = routes.trip_start.apply(
-        lambda t: t.strftime('%A'))
-routes['hour'] = routes.trip_start.apply(
-        lambda t: t.hour)
-routes['weekday_num'] = routes.trip_start.apply(
-        lambda t: t.weekday)
-routes['trip_start_time'] = routes.trip_start.dt.time
-routes['time_of_day'] = routes.trip_start.apply(
-        lambda t: t.strftime('%H%M%S')).astype('int')
-routes['duration_minutes'] = routes.duration.apply(
-        lambda t: t.seconds/60.0)
-
-# routes['duration_category'] = pd.cut(
-#         routes.duration_minutes, 
-#         bins=7,
-#         labels=['x_short', 'shorter', 'short', 'normal', 'long', 'longer', 'x-long'],
-#         include_lowest=True,)
+    # Sample group of data points for a single vehicle
+    # k, v = list(vehicles)[2]
+    # k # vehicle_id
+    # v # dataframe
 
 
-# Sort
-routes = routes.sort_values('trip_start')
+    # all_routes_dataframes = []
+    # all_blocks = set()
+    # 
+    # # @TODO for later
+    # for k, v in vehicles:
+
+    def stats_for_vehicle_groups(df):
+        df = df.sort_values('event_timestamp')
+
+        # Save only the rows where the situation/state changed (approaching => leaving)
+        state_changes = (df != df.shift(1)).situation
+        vroutes = df.loc[state_changes].copy()
+
+        previous_rows = vroutes.shift(1)
+
+        # Save the start time, end time & duration of each trip
+        vroutes['trip_start'] = previous_rows.event_timestamp # Time leaving first stop
+        vroutes['trip_end'] = vroutes.event_timestamp # Time leaving last stop
+        vroutes['duration'] = vroutes.trip_end - vroutes.trip_start
+
+        vroutes['delay_start'] = previous_rows.delay 
+        vroutes['delay_end'] = vroutes.delay
+
+        # Save the block ID for the start and end of each trip
+        # These should always be the same, otherwise something is wrong
+        # with that data point.
+        vroutes['block_start'] = previous_rows.blockID.astype('str')
+        vroutes['block_end'] = vroutes.blockID.astype('str')
+
+        # Calculte the distance between each start and and point
+        # This should help validate data as well 
+        vroutes['lat_start'] = previous_rows.vehicle_location_latitude
+        vroutes['lon_start'] = previous_rows.vehicle_location_longitude
+        vroutes['lat_end'] = vroutes.vehicle_location_latitude
+        vroutes['lon_end'] = vroutes.vehicle_location_longitude
+        vroutes['distance'] = vroutes.apply(get_distance, axis=1)
+
+        # Only keep the "ending" data points for each trip
+        completed_routes = vroutes[vroutes.situation.str.contains('last')]
+
+        # Only keep columns we are interested in, and order them
+        completed_routes = completed_routes[[
+                'duration',
+                'distance',
+                'delay', # at last stop observation
+                'trip_start',
+                'trip_end',
+                'block_start',
+                'block_end',
+                'vehicle_id',
+                'type',
+                'tripID',
+                'situation',
+                'routeNumber',
+                'direction',
+                'bearing',
+                'lastLocID',
+                'lastStopSeq',
+                'loadPercentage', # at last stop observation
+                ]]
+
+        # print("Sample for vehicle #{}".format(k))
+        # print(completed_routes)
+
+        # all_routes_dataframes.append(completed_routes)
+        return completed_routes
+
+    # routes = pd.concat(all_routes_dataframes)
+    routes = leavings.groupby('vehicle_id', group_keys=False).apply(stats_for_vehicle_groups)
+    # unstack()?
+
+    # Drop rows with NA durations
+    routes = routes.dropna()
+
+    # Drop outliers
+    # about 50 out of 4000 are over 100 minutes
+    # @TODO look more into these later
+    # routes = routes[routes.duration < dt.timedelta(seconds=(60*100))]
+    # routes = routes[scipy.stats.zscore(routes.duration) < 3]
+    # These are generally over 4 hours or event multiple days
+    routes = routes[routes.duration < routes.duration.quantile(0.95)]
+
+    # Add new columns for grouping & aggregating 
+    routes['weekday'] = routes.trip_start.apply(
+            lambda t: t.strftime('%A'))
+    routes['hour'] = routes.trip_start.apply(
+            lambda t: t.hour)
+    routes['weekday_num'] = routes.trip_start.apply(
+            lambda t: t.weekday)
+    routes['trip_start_time'] = routes.trip_start.dt.time
+    routes['time_of_day'] = routes.trip_start.apply(
+            lambda t: t.strftime('%H%M%S')).astype('int')
+    routes['duration_minutes'] = routes.duration.apply(
+            lambda t: t.seconds/60.0)
+
+    # routes['duration_category'] = pd.cut(
+    #         routes.duration_minutes, 
+    #         bins=7,
+    #         labels=['x_short', 'shorter', 'short', 'normal', 'long', 'longer', 'x-long'],
+    #         include_lowest=True,)
+
+
+    # Sort
+    routes = routes.sort_values('trip_start')
 
 
 
-print(routes.duration.describe())
-# print()
-# print(routes.pivot_table(index=['weekday', 'hour'], values='duration', aggfunc='mean'))
+    print(routes.duration.describe())
+    # print()
+    # print(routes.pivot_table(index=['weekday', 'hour'], values='duration', aggfunc='mean'))
 
-fname = 'completed_bus_routes.pickle'
-print("DataFrame saved to '{}'".format(fname))
-routes.to_pickle(fname)
+    if save:
+        route_number = routes.routeNumber.mode()[0] # Should be just one route number!!
+        # @TODO add trip start and end stops
+        fname = "completed_bus_routes-bus{}.pickle".format(route_number)
+        print("DataFrame saved to '{}'".format(fname))
+        routes.to_pickle(fname)
+
+    return routes
 
 # import ipdb; ipdb.set_trace()
 
@@ -233,3 +242,9 @@ routes.to_pickle(fname)
 @TODO collect all trip lengths and do histogram
 see what variables describe variance
 """
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+    data = pd.read_json(args.path_to_bus_trip_json_data)
+    main(data, save=True)
