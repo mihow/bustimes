51268c9ab91cae046ebc0ef43139b7edb4f326d2
Michael Bunsen <notbot@gmail.com>
2018-08-05 11:06:11 -0700

    Script for fetching some bus times data from AWS S3.

---
+++ fetch_sample_from_s3.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import boto3
+import ast
+import pprint
+import json
+
+
+sample = []
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('bustimes-data')
+search = bucket.objects.filter(Prefix='raw/bustimes__2018')
+for obj in search.limit(10):
+    print("Fetching", obj.key)
+    data_raw = obj.get()['Body'].read().decode('utf-8')
+    data = ast.literal_eval(data_raw)
+    # pprint.pprint(data, indent=2)
+    sample += data
+
+fname = 'data/bustimes_sample.json'
+f = open(fname, 'w')
+json.dump(sample, f, indent=2)
+
+print("Saved to", fname)
