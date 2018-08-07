f18f827c783d39a55340c71a65b978a955e34825
Michael Bunsen <notbot@gmail.com>
2018-08-06 23:27:27 -0700

    Color vehicles on the same route. Index by sign message.

--- bustimes_dash.py
+++ bustimes_dash.py
@@ -23,8 +23,8 @@ def timestamp_to_dt(t, format=False):
     return dt
 
 def bus_route_choices():
-    routes = list(data['routeNumber'].unique())
-    choices = [{'label': "Bus #{}".format(r), 'value': r} for r in routes]
+    routes = sorted([r for r in list(data['signMessageLong'].unique()) if r])
+    choices = [{'label': "{}".format(r), 'value': r} for r in routes]
     return choices
 
 
@@ -75,55 +75,78 @@ app.layout = html.Div([
     [Input('bus_routes', 'value')],
 )
 def update_summary(bus_route):
-    route_data = data[data.routeNumber == bus_route]
-    summary = make_summary(route_data)
+    if bus_route:
+        route_data = data[data['signMessageLong'] == bus_route]
+        summary = make_summary(route_data)
 
-    list_items = [html.Li("{}: {}".format(k, v)) for k, v in summary.items()]
+        list_items = [html.Li("{}: {}".format(k, v)) for k, v in summary.items()]
 
-    return list_items
+        return list_items
+    else:
+        return None
 
 @app.callback(
     Output('bus_positions', 'figure'),
     [Input('bus_routes', 'value')],
 )
 def update_bus_positions(bus_route):
-    route_data = data[data.routeNumber == bus_route]
+    route_data = data[data['signMessageLong'] == bus_route]
     description = ", ".join([
         msg for msg in route_data.signMessage.unique() if msg])
 
     def hover_text(row):
         date = timestamp_to_dt(row['time']).strftime(date_fmt)
-        name = row['signMessageLong']
+        name = row['vehicleID']
         text = "{}<br>{}".format(name, date)
         return text
 
-    figure = {
-        'data': [
-            {
-                'lon': route_data['longitude'],
-                'lat': route_data['latitude'],
+    def get_position_index(time, start, end):
+       # Value between 0 and 1
+       pos = (time - start) / (end - start)
+       return pos
+
+    if bus_route:
+        chart_data = []
+        for vehicle, route in route_data.groupby('vehicleID'):
+            start = route['time'].min()
+            end = route['time'].max()
+
+            chart_data.append({
+                'lon': list(route['longitude']),
+                'lat': list(route['latitude']),
                 #'color': route_data['tripID'].as_type(int),
-                'text': route_data.apply(hover_text, axis=1),
+                'marker': {
+                    'opacity': list(route['time'].apply(
+                        get_position_index,
+                        args=[start, end]) + 0.3)
+                    #'size': list(route_data['delay']),
+                },
+                'text': list(route.apply(hover_text, axis=1)),
+                'name': vehicle,
                 'hoverinfo': 'text',
                 'type': 'scattermapbox',
                 'mode': 'markers',
-            },
-        ],
-        'layout': {
-            'autosize': True,
-            'mapbox': {
-                'accesstoken': mapbox_token,
-                'zoom': 10,
-                'center': {
-                    # Portland center
-                    'lat': route_data['latitude'].median(),
-                    'lon': route_data['longitude'].median()
+            })
+
+        figure = {
+            'data': chart_data,
+            'layout': {
+                'autosize': True,
+                'mapbox': {
+                    'accesstoken': mapbox_token,
+                    'zoom': 10,
+                    'center': {
+                        # Portland center
+                        'lat': route_data['latitude'].median(),
+                        'lon': route_data['longitude'].median()
+                    },
                 },
+                'title': description,
             },
-            'title': description,
-        },
-    }
-    return figure
+        }
+        return figure
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run_server(debug=True)
